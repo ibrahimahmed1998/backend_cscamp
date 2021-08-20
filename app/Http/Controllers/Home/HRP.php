bbbe9dcc9fd8a534cfe8df570dd89658fa06759d
Mohammad Essam <ongoingprodigy@gmail.com>
2021-08-20 15:46:47 -0400

    handling some erros

--- app/Http/Controllers/Home/HRP.php
+++ app/Http/Controllers/Home/HRP.php
@@ -11,12 +11,15 @@ class HRP extends Controller{ // Class Name : Home Random Posts
         $user = User::where('api_token', $req->bearerToken())->first();
         $user_posts = Post::where('user_id', $user->id)->get();
         $star = $user->following->all();
-
+        $stars[] = null;
         foreach ($star as $key => $pivot){
-            $stars[] =  $pivot->pivot->publisher_id;   }
+            $stars[] =  $pivot->pivot->publisher_id;
+           }
 
         for ($i = 0; $i < count($stars); $i++){
             $Star_posts[]  = Post::where('user_id', $stars[$i])->get(); }
 
         $magic_posts = Arr::collapse([$user_posts, $Star_posts]);
-        return  $magic_posts;      }}
+        return  $magic_posts;      
+    }
+}
